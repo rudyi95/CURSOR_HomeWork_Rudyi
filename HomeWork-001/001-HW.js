032ecd5a45a6ed497a07196d5379413c337b840f
rudyi95 <rud11@meta.ua>
2020-01-31 16:59:07 +0200

    HomeWork-001_Variables_and_Types

---
+++ HomeWork-001/001-HW.js
@@ -0,0 +1,55 @@
+const Nokia = 15.678, Samsung = 90.2345, Apple = 123.965;
+let maxPrice, minPrice, summOfPrices, summOfPricesFloor, summOfPricesFix, summOfPricesBool, a, remainder, remainderA, 
+clientPay = 500, averagePrice, randomDiscount = Math.random(), someGood = 0, priceWithDiscount, lostProfit;
+
+// 1)
+maxPrice = Math.max(Nokia,Samsung,Apple);
+
+// 2)
+minPrice = Math.min(Nokia,Samsung,Apple);
+
+// 3)
+summOfPrices = Nokia + Samsung + Apple;
+
+// 4)
+summOfPricesFloor = Math.floor(Nokia) + Math.floor(Samsung) + Math.floor(Apple);
+
+// 5)
+summOfPricesFix = summOfPrices.toFixed(1);
+
+// 6)
+summOfPricesBool = summOfPricesFloor;
+if(summOfPricesBool % 2 == 0) {
+    a = "Even";
+} else {
+    a = "Odd";
+}
+
+// 7)
+if(summOfPrices <= clientPay){
+    remainder = clientPay - summOfPrices;
+    remainderA = "Remainder is: " + remainder;
+}else{
+    remainder = 1*(summOfPrices - clientPay);
+    remainderA = "You have to give " + remainder + " more";
+}
+
+// 8)
+averagePrice = (summOfPrices/3).toFixed(2);
+
+// 9)
+someGood = prompt("Write price of your good!", someGood);
+priceWithDiscount = someGood - randomDiscount;
+lostProfit = someGood / 2 - randomDiscount;
+
+console.log(`
+Max price is: ${maxPrice} , 
+Min price is: ${minPrice} , 
+Summ of prices: ${summOfPrices}, 
+Summ of prices to floor: ${summOfPricesFloor},
+Summ of prices fixed: ${summOfPricesFix},
+Summ of prices is: ${a},
+${remainderA},
+Average price: ${averagePrice},
+Lost profit: ${lostProfit}
+`);
