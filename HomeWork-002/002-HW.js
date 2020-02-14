4adfc6a13779efe83ebcc819d87f94e1f9f83ff5
rudyi95 <rud11@meta.ua>
2020-02-14 11:42:21 +0200

    added alerts

--- HomeWork-002/002-HW.js
+++ HomeWork-002/002-HW.js
@@ -3,51 +3,51 @@ let numberN, numberM, verN, verM, verIntN, verIntM, missNum, numbers = 0, answer
 //1)
 numberN = prompt("Write number N ", 1);
 
-if(numberN == null){
+if (numberN == null) {
     numberN = 'sda';
     alert("You clicked cancel!");
-}else if(numberN == ''){
+} else if (numberN == '') {
     numberN = 'sda'
     alert("You didn't write anything!");
 }
 
 verN = +numberN;
-    while(!Number.isInteger(verN)){
-        alert("Write integer please!!!");
-        numberN = prompt("Write number N again", 1);
+while (!Number.isInteger(verN)) {
+    alert("Write integer please!!!");
+    numberN = prompt("Write number N again", 1);
 
-        if(numberN == null){
-            numberN = 'sda';
-            alert("You clicked cancel!");
-        }else if(numberN == ''){
-            numberN = 'sda'
-            alert("You didn't write anything!");
-        }
-        verN = +numberN;
+    if (numberN == null) {
+        numberN = 'sda';
+        alert("You clicked cancel!");
+    } else if (numberN == '') {
+        numberN = 'sda'
+        alert("You didn't write anything!");
     }
+    verN = +numberN;
+}
 
 /*-------------------------------------------------------*/
 
 //2)
 numberM = prompt("Write number M ", 10);
 
-if(numberM == null){
+if (numberM == null) {
     numberM = 'sda';
     alert("You clicked cancel!");
-}else if(numberM == ''){
+} else if (numberM == '') {
     numberM = 'sda'
     alert("You didn't write anything!");
 }
 
 verM = +numberM;
-while(!Number.isInteger(verM)){
+while (!Number.isInteger(verM)) {
     alert("Write integer please!!!");
     numberM = prompt("Write number M again", 10);
 
-    if(numberM == null){
+    if (numberM == null) {
         numberM = 'sda';
         alert("You clicked cancel!");
-    }else if(numberM == ''){
+    } else if (numberM == '') {
         numberM = 'sda'
         alert("You didn't write anything!");
     }
@@ -58,22 +58,22 @@ while(!Number.isInteger(verM)){
 
 //3)
 missNum = confirm("Miss even numbers? ");
-if(missNum == true){
+if (missNum == true) {
     answer = "YES!";
-}else{
+} else {
     answer = "NO!";
 }
-if(missNum == false){
-    for(let i = verN; i <= verM; i++){
-            numbers += i;
-        }       
-    }else{
-        for(let j = verN; j <= verM; j++){
-            if(j % 2 != 0) {
-                numbers += j;
-            }
+if (missNum == false) {
+    for (let i = verN; i <= verM; i++) {
+        numbers += i;
+    }
+} else {
+    for (let j = verN; j <= verM; j++) {
+        if (j % 2 != 0) {
+            numbers += j;
         }
     }
+}
 
 
 /*-------------------------------------------------------*/
